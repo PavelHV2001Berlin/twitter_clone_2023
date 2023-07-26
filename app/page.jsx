411c663fa5dff1ca5ae42e578e5fd6dcabb32e30
PavelHV2001Berlin <p_volkmann@outlook.de>
2023-07-26 17:06:31 +0200

    add H3

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1>Das ist eine neue überschrift</h1><h2>Eine h2</h2></div>
+    <div><h1>Das ist eine neue überschrift</h1><h2>Eine h2</h2><h3>Eine H3</h3></div>
   )
 }
 
