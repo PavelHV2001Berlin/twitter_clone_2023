e7347c8c53e298d30b346a35170176e76f746ee6
PavelHV2001Berlin <p_volkmann@outlook.de>
2023-07-28 13:02:45 +0200

    Login screen vorbereiten

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -15,9 +15,7 @@ const Home = () => {
         <div className="create_tweet_container">
           <Image className='profile_image' src="/assets/images/testimage.jpg" height={50} width={50} alt='Profilbild'/>
           <div className='tweet_input_area'>
-            <p>
-              What is happening
-            </p>
+            <input placeholder='What is happening?'/>
             <div className='input_actions_container'>
               <div>
                 <svg fill='rgb(29, 155, 240)' xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h600q24 0 42 18t18 42v600q0 24-18 42t-42 18H180Zm0-60h600v-600H180v600Zm56-97h489L578-473 446-302l-93-127-117 152Zm-56 97v-600 600Zm160.118-390Q361-570 375.5-584.618q14.5-14.617 14.5-35.5Q390-641 375.382-655.5q-14.617-14.5-35.5-14.5Q319-670 304.5-655.382q-14.5 14.617-14.5 35.5Q290-599 304.618-584.5q14.617 14.5 35.5 14.5Z"/></svg>
